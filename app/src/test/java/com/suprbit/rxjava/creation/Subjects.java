245ccf7be836e23b352a6ed1a3f64113b4194225
Yasir Khan Janjua <yasir@suprbit.com>
2017-02-14 16:36:12 +0000

    ReplaySubject, BehaviourSubject and AsyncSubject.

--- app/src/test/java/com/suprbit/rxjava/creation/Subjects.java
+++ app/src/test/java/com/suprbit/rxjava/creation/Subjects.java
@@ -4,8 +4,6 @@ package com.suprbit.rxjava.creation;
 import android.view.View;
 import android.widget.Button;
 
-import com.suprbit.rxjava.TestUtils;
-
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
@@ -13,6 +11,8 @@ import org.mockito.MockitoAnnotations;
 
 import rx.functions.Action1;
 import rx.observers.TestSubscriber;
+import rx.subjects.AsyncSubject;
+import rx.subjects.BehaviorSubject;
 import rx.subjects.PublishSubject;
 import rx.subjects.ReplaySubject;
 
@@ -66,14 +66,14 @@ public class Subjects {
 	public void publish_subject_emits_items_to_all_subscribers_as_they_arrive() {
 		final PublishSubject<Integer> subject = PublishSubject.create();
 
-		TestSubscriber<Integer> subOne = new TestSubscriber<>();
+		final TestSubscriber<Integer> subOne = new TestSubscriber<>();
 		subject.subscribe(subOne);
 
 		subject.onNext(1);
 		subject.onNext(2);
 		subject.onNext(3);
 
-		TestSubscriber<Integer> subTwo = new TestSubscriber<>();
+		final TestSubscriber<Integer> subTwo = new TestSubscriber<>();
 		subject.subscribe(subTwo);
 
 		subject.onNext(4);
@@ -102,41 +102,88 @@ public class Subjects {
 	}
 
 	@Test
-	public void replay_subject_replays_all_items_for_each_subscriber() {
+	public void replay_subject_caches_and_replays_all_items_for_each_subscriber() {
 		ReplaySubject<Integer> subject = ReplaySubject.create();
 
-		subject.subscribe(new Action1<Integer>() {
-			@Override
-			public void call(Integer n) {
-				System.out.println("Subscriber 1 item = " + n);
-			}
-		});
+		final TestSubscriber<Integer> subOne = new TestSubscriber<>();
+		subject.subscribe(subOne);
 
 		subject.onNext(1);
 		subject.onNext(2);
 		subject.onNext(3);
 
-		subject.subscribe(new Action1<Integer>() {
-			@Override
-			public void call(Integer n) {
-				System.out.println("Subscriber 2 item = " + n);
-			}
-		});
+		final TestSubscriber<Integer> subTwo = new TestSubscriber<>();
+		subject.subscribe(subTwo);
 
 		subject.onNext(4);
 		subject.onNext(5);
 
-		subject.subscribe(new Action1<Integer>() {
-			@Override
-			public void call(Integer n) {
-				System.out.println("Subscriber 3 item = " + n);
-			}
-		});
+		subject.onCompleted();
+
+		final TestSubscriber<Integer> subThree = new TestSubscriber<>();
+		subject.subscribe(subThree);
+
+		subOne.assertValueCount(5);
+		subTwo.assertValueCount(5);
+		subThree.assertValueCount(5);
+	}
+
+	@Test
+	public void replay_subject_limit_caching() {
+		final ReplaySubject<Integer> replaySubject = ReplaySubject.createWithSize(2);
+
+		final TestSubscriber<Integer> testOne = new TestSubscriber<>();
+		replaySubject.subscribe(testOne);
+
+		replaySubject.onNext(1);
+		replaySubject.onNext(2);
+		replaySubject.onNext(3);
+		replaySubject.onNext(4);
+		replaySubject.onNext(5);
+
+		final TestSubscriber<Integer> testTwo = new TestSubscriber<>();
+		replaySubject.subscribe(testTwo);
+
+		testOne.assertValueCount(5);
+		testTwo.assertValueCount(2);
+	}
+
+	@Test
+	public void behaviour_subject_caches_last_event() {
+		final BehaviorSubject<Integer> behaviorSubject = BehaviorSubject.create();
+		behaviorSubject.onNext(1);
+		behaviorSubject.onNext(2);
+		behaviorSubject.onNext(3);
+		behaviorSubject.onNext(4);
+		behaviorSubject.onNext(5);
+
+		final TestSubscriber<Integer> subOne = new TestSubscriber<>();
+		behaviorSubject.subscribe(subOne);
+		subOne.assertValueCount(1);
+
+		behaviorSubject.onCompleted();
+
+		final TestSubscriber<Integer> subTwo = new TestSubscriber<>();
+		behaviorSubject.subscribe(subTwo);
+		// No value event as the last event was an onComplete.
+		subTwo.assertValueCount(0);
+		subTwo.assertCompleted();
+	}
+
+	@Test
+	public void async_subject_caches_and_emits_last_value() {
+		final AsyncSubject<Integer> asyncSubject = AsyncSubject.create();
+		asyncSubject.onNext(1);
+		asyncSubject.onNext(2);
+		asyncSubject.onNext(3);
+		asyncSubject.onNext(4);
+		asyncSubject.onNext(5);
+		asyncSubject.onCompleted();
 
 		final TestSubscriber<Integer> testSubscriber = new TestSubscriber<>();
-		subject.subscribe(testSubscriber);
-		testSubscriber.assertReceivedOnNext(TestUtils.numbersInRange(1, 5));
-		subject.onCompleted();
+		asyncSubject.subscribe(testSubscriber);
+
+		testSubscriber.assertValueCount(1);
 	}
 
 }
